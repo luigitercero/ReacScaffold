67205c449272de604bc38b462c9d387735865791
luigitercero <luigitercero3@gmail.com>
2018-10-13 18:10:37 -0600

    conectando con FireBase

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,67 @@
 import React from 'react';
-import request from 'superagent'
+import request from 'superagent';
+import firebase from 'firebase';
 class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      users: []
+      users: [],
+
     }
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogOut =this.handleLogOut.bind(this);
+    this.renderLoginButton = this.renderLoginButton.bind(this);
   }
 
-
+    componentWillMount() {
+      firebase.auth().onAuthStateChanged(user=>{
+        this.setState({user})
+      })
+    }
     componentDidMount() {
         request
         .get('//localhost:3001/api/users')
         .end((err,res)=> {
             const users = JSON.parse(res.text).users;
             this.setState({
-                users:users
+                users:users,
+                user:null
             })
         })
     }
+    handleAuth() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider)
+      .then(result =>console.log(`${result.user.email} ha iniciado sesion`) )
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+    }
 
+    handleLogOut() {
+      firebase.auth().signOut()
+      .then(result =>console.log(`${result.user.email} ha salido`) )
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));;
+    }
+    renderLoginButton() {
+      if (this.state.user){
+        return(
+        <div>
+          <img src = {this.state.user.photoURL} alt = {this.state.user.displayName}/>
+          <p>hola {this.state.user.displayName}</p>
+          <button onClick= {this.handleLogOut}>LogOut</button>
+        </div>);
+      }else {
+        return (
+        <button onClick= {this.handleAuth}>Login con google</button>
+        );
+      }
+    }
     render() {
-     
         var users = this.state.users.map((user) => {
           return <li >{user.nombre}</li>
         });
         return(
           <div>
+            {this.renderLoginButton()}
             <h1>Mi Aplicación Modificada</h1>
             <ul>
               {users}
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App.jsx'
-class Prueba extends React.Component {
-  
-    render () {
-        return (<h1>hola</h1>)
-    }
-}
+import firebase from 'firebase'
 
+firebase.initializeApp({
+    apiKey: "AIzaSyBNTB32pE-4sp7K1O0xV0sbU8ip68X5vdM",
+    authDomain: "seudogram-b6c53.firebaseapp.com",
+    databaseURL: "https://seudogram-b6c53.firebaseio.com",
+    projectId: "seudogram-b6c53",
+    storageBucket: "seudogram-b6c53.appspot.com",
+    messagingSenderId: "540425625946"
+})
 render (
     <App/>,document.getElementById('app')
 );
